d10941a23a781c48d152ffe2033bb86a830853bb
hunghoang1 <hunghoang1@tamdongtam.vn>
2019-10-29 18:31:57 +0700

    registercommit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, RequiredValidator } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'register-project';
+  myForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  private buildForm() {
+    this.myForm = this.formBuilder.group({
+      username: [undefined, Validators.required],
+      password: [undefined, [Validators.required, Validators.minLength(5)]],
+      cfPassword: [undefined],
+      email: [undefined, Validators.email],
+      location: [undefined, Validators.maxLength(255)],
+      phone: [undefined],
+      note: [undefined, Validators.maxLength(255)],
+    }) 
+  }
+
+  submitForm() {
+    console.log(this.myForm.get('location'));
+    // console.log(this.myForm.get('password'))
+  }
+
+  resetForm() {
+    this.myForm.reset();
+  }
 }
